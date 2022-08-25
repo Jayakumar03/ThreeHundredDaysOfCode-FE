de265443853306fa4c0c9b597cfeaebf9052f8bd
Ravi Tandon <ravi@decoverhq.com>
2022-08-25 23:08:45 +0000

    Feed - WIP.

--- src/components/CodeCard.js
+++ src/components/CodeCard.js
@@ -1,9 +1,11 @@
 import { Card, Input } from 'antd';
+import { Avatar, Image } from 'antd';
 import React from 'react';
-
 import { LikeOutlined, CommentOutlined } from '@ant-design/icons';
+import CommentBox from '../components/CommentBox';
 
-function CodeCard() {
+function CodeCard(props) {
+  console.log(props.data);
 
 function submitComment() {}
 function submitLike() {}
@@ -16,11 +18,20 @@ return (
         width: 500,
       }}
     >
-      <div>        
-        <p> Author: Ravi Tandon </p>
+      <div className='content-card-author-container'>
+        <Avatar className='comment-avatar' src="https://joeschmoe.io/api/v1/random" />
+        <p className='content-card-author-name'> {props.data.data.name} </p>
       </div>
       <div className='code-block'>
-        <p>Card content</p>
+        <p>{props.data.data.codeBlock}</p>
+      </div>
+      <div className='reaction-stats-box'>
+      <div>
+        <p>{props.data.data.numLikes} Likes</p>
+      </div>
+      <div>
+        <p>{props.data.data.numComments} Comments</p>
+      </div>
       </div>
       <hr className='line-class'></hr>
         <div className='reaction-block'>
@@ -34,7 +45,10 @@ return (
         </div>        
         </div>
       <hr className='line-class'></hr>
-      <Input 
+      {props.data.data.comments.map((result) => (
+       <CommentBox text={result.text} author={result.author}/>
+      ))}
+      <Input
         placeholder='Write a comment...'      
         className='reaction-comment-box'>            
         </Input>
