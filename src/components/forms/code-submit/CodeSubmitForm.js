f3687c49ebe6795c433f6422ab7b3a108f3a179d
Satyam Shivam Sundaram <satyspy007@gmail.com>
2022-09-28 13:03:14 +0530

    [UI][BUGFIX] Code submit modal
    Changes include-
    1. Fixed redirection to /home on refresh or opening new window
    2. Code Submit Modal

--- src/components/forms/code-submit/CodeSubmitForm.js
+++ src/components/forms/code-submit/CodeSubmitForm.js
@@ -1,70 +1,38 @@
 // Elements.
-import { Button, Input, Modal } from 'antd';
-
-import { Navigate, useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import { Button } from 'antd';
+import React, { useState } from 'react';
 
 // Styles.
 import './CodeSubmitForm.css';
+import CodeSubmitModal from './CodeSubmitModal';
 
-function CodeSubmitForm(props) {    
-    const navigate = useNavigate();
-    function handleNewProblemClick() {
-        document.getElementById('code-submit-form-elements').style.display = 'block';
-    }
-    function handleClick() {
-        let problemTitle = document.getElementById('code-submit-form-input-problem-name').value;
-        if (problemTitle === '') { 
-            problemTitle = props.problem.problemTitle;
-        }
-        let problemLink = document.getElementById('code-submit-form-input-problem-link').value
-        if (problemLink === '') {
-            problemLink = props.problem.problemLink;
-        }
-        const solutionLink = document.getElementById('code-submit-form-input-github-link').value;
-        const values = {
-            problemTitle: problemTitle,
-            problemLink: problemLink,
-            solutionLink: solutionLink
-          };
-        props.handleClick(values);
-
-        document.getElementById('code-submit-form-elements').style.display = 'none';
+function CodeSubmitForm(props) {
+    // modal toggle
+    const [openCodeModal, setOpenCodeModal] = useState(false);
+    const toggleCodeModal = () => {
+      setOpenCodeModal(prev => {
+        return !prev
+      });
     }
 
-    function getProblemSubmissions() {
-        const problemId = props.problemId;        
-        navigate("/problemSubmissions/" + problemId);
-    }
-
+    const getProblemSubmissions = () => {}
 
     return (
         <div className='code-submit-form-container'>
-        <div className='code-submit-form-inputs'>
-            <div className='code-submit-form-elements' id='code-submit-form-elements'>
-                <div className='code-submit-form-input-wrapper'>
-                    <p className='code-submit-form-input-name'>Problem Link:</p>
-                    <Input className='code-submit-form-input' id='code-submit-form-input-problem-link' placeholder={props.problem.problemLink} />
+            <div className='code-submit-form-inputs'>
+                <CodeSubmitModal
+                    isOpen={openCodeModal}
+                    onClose={toggleCodeModal}
+                    problem={props.problem}
+                    handleSubmit={props.submitSolution}
+                >
+                </CodeSubmitModal>
+                <div className='code-submit-buttons'>
+                    <Button onClick={toggleCodeModal}> Submit Github Link </Button>
+                    <Button onClick={getProblemSubmissions}> Submissions </Button>
                 </div>
-                <div className='code-submit-form-input-wrapper'>
-                    <p className='code-submit-form-input-name'>Problem Name:</p>
-                    <Input className='code-submit-form-input' id='code-submit-form-input-problem-name' placeholder={props.problem.problemTitle} />
-                </div>    
-                <div className='code-submit-form-input-wrapper'>
-                    <p className='code-submit-form-input-name'>Github Link:</p>
-                    <Input className='code-submit-form-input' id='code-submit-form-input-github-link' placeholder="Github Link"/>
-                </div>
-                <div className='code-submit-form-input-wrapper'>
-                    <Button className='code-submit-form-submit-button' id='code-submit-form-save-button' onClick={handleClick}> Save </Button>
-                </div>
-            </div>
-            <div className='code-submit-buttons'>
-                <Button className='code-submit-form-submit-button' onClick={handleNewProblemClick}> Submit Github Link </Button>
-                <Button className='code-submit-form-submit-button' onClick={getProblemSubmissions}>Submissions </Button>
             </div>
         </div>
-    </div>
-    
     );
 }
 
